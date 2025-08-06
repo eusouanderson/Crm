a37fb41da07c6272f0d3da3648100ab8a76b3e38
Anderson Rodrigues <eusouanderson@outlook.com>
2025-08-05 23:26:51 -0300

    Seed implanted

---
+++ backend/scripts/seed.ts
@@ -0,0 +1,10 @@
+import { users } from '@/database/schema/users';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { seed } from 'drizzle-seed';
+
+async function main() {
+  const db = drizzle(process.env.DATABASE_URL);
+  await seed(db, { users });
+}
+
+main();
